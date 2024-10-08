a8961f664d2e7dfcab9140eb037f5263a22c1cdf
Abdill <dmohammed@hotmail.co.uk>
2024-10-08 22:12:59 +0100

    Created a ButcherFactory to act as a superfactory for all varieties of meats

--- Models/Factories/Concrete/ButcherShopFactory.cs
+++ Models/Factories/Concrete/ButcherShopFactory.cs
@@ -1,6 +1,32 @@
-﻿namespace AldyarOnlineShoppig.Models.Factories.Concrete
+﻿using AldyarOnlineShoppig.Models.Enums;
+using AldyarOnlineShoppig.Models.Factories.Abstract;
+using AldyarOnlineShoppig.Models.Interfaces;
+
+namespace AldyarOnlineShoppig.Models.Factories.Concrete
 {
     public class ButcherShopFactory
     {
+        /* The main purpose of this class is to act as a "superfactory" or "abstract factory" that manages 
+           and coordinates the creation of different types of meat products across various meat categories 
+           regardless of the specific meat type .
+        */
+        public IMeatProduct CreateMeatProduct(MeatType type, Enum cut, double weight)
+        {
+            MeatFactory factory = GetFactory(type);
+            return factory.CreateMeatProduct(cut, weight);
+        }
+        private MeatFactory GetFactory(MeatType type)
+        {
+            switch (type)
+            {
+                case MeatType.Beef:
+                    return new BeefFactory();
+                case MeatType.Chicken:
+                    return new BeefFactory();
+                // Add other meat type factories here
+                default:
+                    throw new ArgumentException("Invalid meat type", nameof(type));
+            }
+        }
     }
 }
